3f4f36a765185be984b0c2970f433996279e231c
svofski <svofski@localhost>
2010-07-30 00:09:03 +0400

    gpio -> ++

--- src/dbot/eaboard/source/leds.cpp
+++ src/dbot/eaboard/source/leds.cpp
@@ -20,10 +20,6 @@ void startLEDFlashTasks(unsigned portBASE_TYPE uxPriority) {
     for (i = 0; i < 2; i++) {
         xTaskCreate(ledFlashTask, (signed char *) "LEDx", configMINIMAL_STACK_SIZE, NULL, uxPriority,  (xTaskHandle *)NULL);
     }
-    gpio_set_led(0,1);
-    gpio_set_led(1,1);
-    gpio_set_led(2,1);
-    gpio_set_led(3,1);
 
     xTaskCreate(lcdControlTask, (signed char *)"LCD", configMINIMAL_STACK_SIZE, NULL, uxPriority, (xTaskHandle *)NULL);
 }
@@ -41,39 +37,28 @@ static portTASK_FUNCTION( ledFlashTask, pvParameters )
 
     xLastFlashTime = xTaskGetTickCount();
     for (;;) {
-        gpio_set_led(led_id, 0);
+        (led_id ? &GPIO::led_red : &GPIO::led_green)->On();
         vTaskDelayUntil(&xLastFlashTime, 100/portTICK_RATE_MS);
-        gpio_set_led(led_id, 1);
+        (led_id ? &GPIO::led_red : &GPIO::led_green)->Off();
         vTaskDelayUntil(&xLastFlashTime, xFlashRate);
-        //vTaskDelete(NULL);
     }
 }
 
-#define LCD_RES (1<<25)// LCD_RST#
-#define LCD_CS  (1<<24)// LCD_CS#
-#define LCD_DIO (1<<6) // MOSI
-#define LCD_SCK (1<<4) // CLK
 
+static portTASK_FUNCTION(lcdControlTask, pvParameters) { (void)pvParameters;
+    char buf[64];
 
-static OUT lcdCS   = OUT(LCD_CS, 1);
-static OUT lcdMOSI = OUT(LCD_DIO, 0);
-static OUT lcdSCK  = OUT(LCD_SCK, 0);
-static OUT lcdRES  = OUT(LCD_RES, 1);
-
-char buf[64];
-
-static portTASK_FUNCTION(lcdControlTask, pvParameters) {
     vTaskDelay(500/portTICK_RATE_MS);
 
-    LCD lcd(&lcdCS,
-            &lcdMOSI,
-            &lcdSCK,
-            &lcdRES,
+    LCD lcd(&GPIO::lcdCS,
+            &GPIO::lcdMOSI,
+            &GPIO::lcdCLK,
+            &GPIO::lcdRST,
             &g_font3x5, clrYellow, clrDkBlue);
             
-   for(int i = 0;;i++) {
+   for(int i = 0;; i++) {
         lcd.FillRect(90,10,20,20, i & 0377);
-        lcd.FillCircle(96,64, 1+ (16 - i % 16), 0377 - i & 0377);
+        lcd.FillCircle(96,64, 1+ (16 - i % 16), 0377 - (i & 0377));
 
         xsprintf(buf, "Gruuu %010d", i);
         lcd.Print(buf, 1 + i % 20, FALSE);
