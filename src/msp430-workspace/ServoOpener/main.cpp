411aa2975a77e1323145b8c1d7f441d1e5856725
svofski <svofski@gmail.com>
2012-02-25 13:47:00 +0400

    added IV-16 Numishnik

--- src/msp430-workspace/ServoOpener/main.cpp
+++ src/msp430-workspace/ServoOpener/main.cpp
@@ -92,9 +92,10 @@ void startUpSequence() {
 }
 
  /* itoa:  convert n to characters in s */
- void itoa(int n, char s[])
+ void itoa(int _n, char s[])
  {
      int i, sign;
+     int32_t n = _n;
  
      if ((sign = n) < 0)  /* record sign */
          n = -n;          /* make n positive */
@@ -109,91 +110,477 @@ void startUpSequence() {
 }
 
 
+int atoi(char* s, int* result) 
+{
+    int pos;
+    *result = 0;
+    
+    reverse(s);
+    for(pos = 1; *s != 0; pos*=10, s++) {
+        switch (*s) {
+        case '-': 
+            *result = -*result;
+            return 0;
+        case '+':
+            return 0;
+        default:
+            if (*s >= '0' && *s <= '9') {
+                *result += (*s - '0') * pos;
+                continue;
+            } else {
+                return -1;
+            }
+        }
+    }
+    return 0;
+}
+
+
+void printInt(int x) {
+    char buff[8];
+    itoa(x, buff);
+    com.puts(buff);
+}
+
+
+OpenServo os(0x10);
+
 void probeOS()
 {
-    uint16_t data;
-    char buff[8];
+    uint8_t maj, min;
     
-    OpenServo os(0x10);
+    //OpenServo os(0x10);
     
     i2c.Init();
-    
-    if (os.read16(0x02, &data) == -1) {
-        com.puts("Could not read OpenServo version\n");
+
+    if (os.GetVersion(&maj, &min) != 0) {    
+        com.puts("\nCould not read version\n");
         return;
     }
-    com.puts("OpenServo version:");
-    itoa(data & 0x377, buff);
-    com.puts(buff);
+    com.puts("\nOpenServo version:");
+    printInt(maj);
     com.putchar('.');
-    itoa(data >> 8, buff);
-    com.puts(buff);
+    printInt(min);
     com.puts("\n");
 }
 
-void probePos() {
+uint16_t probeany(int w, OSRegister reg, const char* name, char nl)
+{
     uint16_t data;
-    char buff[8];
-    
-    OpenServo os(0x10);
-    
+
     i2c.Init();
     
-    if (os.read16(0x08, &data) == -1) {
-        com.puts("Could not read OpenServo position\n");
-        return;
+    com.putchar('\n');
+    
+    if ((w ? os.Read16(reg, &data) : os.Read8(reg, reinterpret_cast<uint8_t*>(&data))) != 0) {
+        com.puts("Could not read "); 
+        com.puts(name);
+        com.putchar('\n');
+        return 0177777;
     }
-    com.puts("Position: ");
-    itoa(data, buff);
-    com.puts(buff);
-    com.puts("\n");
+    com.puts(name);
+    com.puts(": ");
+    printInt(w ? data : (data & 0377));
+    
+    com.putchar(nl);
+    
+    return data;
+}
+
+
+uint16_t probe16(OSRegister reg, const char* name, char nl)
+{
+    return probeany(1, reg, name, nl);
+}
+
+uint16_t probe8(OSRegister reg, const char* name, char nl)
+{
+    return probeany(0, reg, name, nl);
+}
+
+
+uint16_t write16(OSRegister reg, uint16_t value)
+{
+    uint16_t data;
+    
+    //OpenServo os(0x10);
+    
+    i2c.Init();
+    os.Write16(reg, value);
+    
+    return 0;
+}
+
+void oscommand(OSCommand c)
+{
+    i2c.Init();
+    os.Command(c);
+}
+
+
+uint16_t probePos() 
+{
+    probe16(POSITION_HI, "position", '\n');
+}
+
+volatile int gruu;
+
+void ndelay() 
+{
+    for (gruu = 0; gruu < 255; gruu++);
+}
+
+void load_pi(int p, int i) 
+{
+    oscommand(WRITE_ENABLE);
+    ndelay();
+    write16(PID_PGAIN_HI, p);
+    ndelay();
+    write16(PID_DGAIN_HI, i);
+    ndelay();
+    oscommand(WRITE_DISABLE);
+}
+
+void setGainFromStr(OSRegister reg, char *cmd) 
+{
+    int value;
+    
+    if (atoi(cmd, &value) == 0) {
+        oscommand(WRITE_ENABLE);
+        ndelay();
+        write16(reg, value);
+        ndelay();
+        oscommand(WRITE_DISABLE);
+        ndelay();
+    }
+}
+
+void setReg8FromStr(OSRegister reg, char *cmd) 
+{
+    int value;
+    
+    if (atoi(cmd, &value) == 0) {
+        oscommand(WRITE_ENABLE);
+        ndelay();
+
+        i2c.Init();
+        os.Write8(reg, (uint8_t)value);
+
+        ndelay();
+        oscommand(WRITE_DISABLE);
+        ndelay();
+    }
+}
+
+
+
+void printGains() 
+{
+    ndelay();
+    probe16(PID_PGAIN_HI, "pid_p", ' ');
+    probe16(PID_IGAIN_HI, "pid_i", ' ');
+    probe16(PID_DGAIN_HI, "pid_d", '\n');
+}
+
+
+void parseCommand(char* cmd) 
+{
+    int state = 0;
+    char *command = cmd;
+    int query = 0;
+    
+    com.puts("\nparsingCommand: '");
+    com.puts(cmd);
+    com.puts("'\n");
+    
+    for(; state != -1; cmd++) {
+        switch (state) {
+        case 0:
+            query = 0;            
+            switch(*cmd) {
+            case 0:
+                state = -1;
+                // make it a query by default
+            case '?':
+                query = 1;
+                // fall through
+            case '=':
+                *cmd = 0;
+                if (strcmp(command, "p") == 0) state = 10;
+                else if (strcmp(command, "d") == 0) state = 20;
+                else if (strcmp(command, "i") == 0) state = 100;
+                else if (strncmp(command, "vel", 3) == 0) state = 30;
+                else if (strncmp(command, "see", 3) == 0) state = 40;
+                else if (strncmp(command, "pow", 3) == 0) state = 50;
+                else if (strncmp(command, "pos", 3) == 0) state = 60;
+                else if (strncmp(command, "pwm", 3) == 0) state = 70;
+                else if (strcmp(command, "reverse") == 0) state = 80;
+                else if (strcmp(command, "reset") == 0) state = 90;
+                else if (strncmp(command, "version", 3) == 0) state = 110;
+                else {
+                    com.puts("unknown command");
+                    state = -1;
+                }
+                if (state != -1 && query) state += 5;  
+                break;
+            default:
+                break;
+            }
+            break;
+        case 10:
+            setGainFromStr(PID_PGAIN_HI, cmd);
+        case 15:
+            printGains();
+            state = -1;
+            break;
+            
+        case 20:
+            setGainFromStr(PID_DGAIN_HI, cmd);
+        case 25:
+            printGains();
+            state = -1;
+            break;
+
+        case 100:
+            setGainFromStr(PID_IGAIN_HI, cmd);
+        case 105:
+            printGains();
+            state = -1;
+            break;
+
+            
+        case 30:
+            setGainFromStr(SEEK_VELOCITY_HI, cmd);
+            // intentional fall-through
+        case 35:
+            probe16(SEEK_VELOCITY_HI, "seek velocity", '\n');
+            state = -1;
+            break;
+        
+        case 40:
+            setGainFromStr(SEEK_POSITION_HI, cmd);
+        case 45:
+            probe16(SEEK_POSITION_HI, "seek", '\n');
+            state = 65; // query position too
+            break;
+            
+        case 50:
+        case 55:
+            probe16(POWER_HI, "power", '\n');
+            state = -1;
+            break;
+            
+        case 60:
+        case 65:
+            probe16(POSITION_HI, "position", '\n');
+            state = -1;
+            break;
+
+        case 70:
+            setGainFromStr(PWM_FREQ_DIVIDER_HI, cmd);
+        case 75:
+            probe16(PWM_FREQ_DIVIDER_HI, "pwm divider", '\n');
+            state = -1;
+            break;
+
+        case 80:
+            setReg8FromStr(REVERSE_SEEK, cmd);
+        case 85:
+            probe8(REVERSE_SEEK, "reverse_seek", '\n');
+            state = -1;
+            break;
+
+        case 90:
+        case 95:
+            oscommand(RESET);
+            com.puts("Device reset\n");
+            state = -1;
+            break;
+            
+        case 110:
+        case 115:
+            probeOS();
+            state = -1;
+            break;
+
+        default:
+            com.puts("error\n");
+            state = -1;
+            break;
+        }
+    }
 }
 
 int main(void)
-{  
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT  
-  systemInit();                             // Init the Board
+{
+    uint16_t pgain = 0, igain = 0, dgain = 0;
+    int input = 0;
+    char buf[32];
+    int bufidx = 0;
+    int pwm = 0;
   
-  startUpSequence();                        // Light up LEDs
+    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT  
+    systemInit();                             // Init the Board
   
-  com.puts("Gruuuu....\n");
+    startUpSequence();                        // Light up LEDs
   
-  while(1) {
-    //Switch2Pressed = 0;
-    __bis_SR_register(LPM2_bits + GIE);
-    __no_operation();			            // For debugger
+    com.puts("Gruuuu....\n");
+  
+    while(1) {
+        __bis_SR_register(LPM2_bits + GIE);
+        __no_operation();			            // For debugger
     
-    
-    while (com.avail()) {
-    	int c = com.getchar();
-    	com.putchar(c);
-        switch (c) {
-            case '1':   probeOS();
-                        break;
-            case '2':   probePos();
-                        break;
-            default:
-                        break;
+        while (com.avail()) {
+        	int c = com.getchar();
+            switch (c) {
+                case '\r':
+                case '\n':  buf[bufidx] = 0;
+                            bufidx = 0;
+                            input = 1;
+                            com.putchar(c);
+                            break;
+                case 010:
+                case 127:
+                            if (bufidx > 0) {
+                                bufidx--;
+                                com.putchar('\\');
+                            }
+                            break;
+                case '`':   oscommand(pwm ? PWM_DISABLE : PWM_ENABLE);
+                            pwm = !pwm;
+                            break;
+                default:    buf[bufidx++] = c;
+                            com.putchar(c);
+                            break;
+            }
+        }
+                    
+        if (input) {
+            input = 0;
+            parseCommand(buf);
+        }
+            
+#if 0            
+                case '1':   probeOS();
+                            break;
+                case '2':   probe16(POSITION_HI, "position");
+                            break;
+                case '3':   probe16(VELOCITY_HI, "velocity");
+                            break;
+                case '4':   probe16(SEEK_POSITION_HI, "seek");
+                            break;
+                case '5':   command(VOLTAGE_READ);
+                            for (gruu = 0; gruu < 255; gruu++);
+                            probe16(POWER_HI, "power");
+                            break;
+                case '6':   probe16(PWM_DIRA, "dira/b");
+                            break;
+                case '7':   probe16(SEEK_VELOCITY_HI, "seek velocity");
+                            break;
+                case '&':   write16(SEEK_VELOCITY_HI, 0x50);
+                            break;
+                case '8':   probe16(FLAGS_HI, "flags");
+                            break;
+                case '9':   
+                schnugg:
+                            ndelay();
+                            pgain = probe16(PID_PGAIN_HI, "pid_p");
+                            igain = probe16(PID_IGAIN_HI, "pid_i");
+                            dgain = probe16(PID_DGAIN_HI, "pid_d");
+                            break;
+                case '0':   write16(SEEK_POSITION_HI, 300);
+                            break;
+                case '-':   write16(SEEK_POSITION_HI, 500);
+                            break;
+                case '=':   write16(SEEK_POSITION_HI, 700);
+                            break;
+                case 'q':   command(PWM_ENABLE);
+                            break;
+                case 'w':   command(PWM_DISABLE);
+                            break;
+                case 'x':   pgain+=1;
+                            command(WRITE_ENABLE);
+                            ndelay();
+                            write16(PID_PGAIN_HI, pgain);
+                            ndelay();
+                            command(WRITE_DISABLE);
+                            goto schnugg;    
+                            break;
+                case 'z':   pgain-=10;
+                            command(WRITE_ENABLE);
+                            ndelay();
+                            write16(PID_PGAIN_HI, pgain);
+                            ndelay();
+                            command(WRITE_DISABLE);
+                            goto schnugg;
+                            break;
+                case 'v':   igain+=10;
+                            command(WRITE_ENABLE);
+                            ndelay();
+                            write16(PID_IGAIN_HI, igain);
+                            ndelay();
+                            command(WRITE_DISABLE);
+                            goto schnugg;    
+                            break;
+                case 'c':   igain-=1;
+                            command(WRITE_ENABLE);
+                            ndelay();
+                            write16(PID_IGAIN_HI, igain);
+                            ndelay();
+                            command(WRITE_DISABLE);
+                            goto schnugg;
+                            break;
+                case 'n':   dgain+=10;
+                            command(WRITE_ENABLE);
+                            ndelay();
+                            write16(PID_DGAIN_HI, dgain);
+                            ndelay();
+                            command(WRITE_DISABLE);
+                            goto schnugg;    
+                            break;
+                case 'b':   dgain-=10;
+                            command(WRITE_ENABLE);
+                            ndelay();
+                            write16(PID_DGAIN_HI, dgain);
+                            ndelay();
+                            command(WRITE_DISABLE);
+                            goto schnugg;
+                            break;
+                case 'A':   com.puts("loading 0x600,0x0c00\n");
+                            load_pi(0x600,0xc00);
+                            goto schnugg;
+                case 'S':   
+                            load_pi(0x400,0xc00);
+                            goto schnugg;
+                case 'D':   
+                            load_pi(0x200,0xc00);
+                            goto schnugg;
+                case 'F':   
+                            load_pi(0x400,-3096);
+                            goto schnugg;
+                default:
+                            com.puts("\nInvalid input\n");
+                            break;
+            }
+        }
+#endif    
+        // Wake up from LPM because user has entered a mode
+        switch(buttons.Selection())
+        {
+          default:
+            // This is not a valid mode
+            // Blink LED1 to indicate invalid entry
+            // Switch S2 was pressed w/o mode select
+            while(buttons.SelectionActive() == 0)
+              {
+                blueleds.Set(7, -1);
+                LongDelay();
+              }
+    	    break;
         }
     }
-    
-    // Wake up from LPM because user has entered a mode
-    switch(buttons.Selection())
-    {
-      default:
-        // This is not a valid mode
-        // Blink LED1 to indicate invalid entry
-        // Switch S2 was pressed w/o mode select
-        while(buttons.SelectionActive() == 0)
-          {
-            blueleds.Set(7, -1);
-            LongDelay();
-          }
-	    break;
-    }
-  }
   
-  return 0;    
+    return 0;    
 }
  
 
