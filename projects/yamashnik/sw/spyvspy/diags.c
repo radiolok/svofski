5b71a96af0bf21df1df4f36a58955f4aad4a1601
svofski <svofski@gmail.com>
2013-06-17 03:51:21 +0400

    a huge mess but it is able to upload COMMAND.COM, serve requested AUTOEXEC.BAT and die when it tries to search first (provided that AUTOEXEC.BAT contains DIR command)

--- projects/yamashnik/sw/spyvspy/diags.c
+++ projects/yamashnik/sw/spyvspy/diags.c
@@ -41,3 +41,29 @@ void eggog(const char* fmt, ...) {
 	va_end(ap);
 	exit(1);
 }
+
+void dumpline(printfunc p, const uint8_t* data, int ofs, int length) {
+	p("%04x  ", ofs);
+	for (int i = 0; i < length; i++) {
+		p("%02x%c", data[ofs+i], i == 7 ? '-':' ');
+	}
+
+	for (int i = 0; i < 16 - length; i++) p("   ");
+
+	p(" ");
+	for (int i = 0; i < length; i++) {
+		p("%c", (data[ofs+i] > 32 && data[ofs+i] < 128) ? 
+			data[ofs+i] : '.');
+	}
+	p("\n");
+}
+
+void dump(printfunc p, const uint8_t* data, int length) {
+	int ofs = 0;
+	for (int line = 0; line < length / 16; line++) {
+		dumpline(p, data, ofs, 16);
+		ofs += 16;
+	}
+	dumpline(p, data, ofs, length % 16);
+}
+
