ac323e2861c583e784b4295b73656ed58c626b6f
svofski <svofski@gmail.com>
2015-01-22 01:57:02 +0300

    p2 tests

--- projects/prettyasm/p2.py
+++ projects/prettyasm/p2.py
@@ -1,83 +1,64 @@
 #!/usr/bin/env python
 
-test1 = "bob: mov a, b 		; fuuu"
-test2 = "mike:mvi c, 'Q' 	; bob"
-test3 = "rob mvi c, 'Q'+8;ommog zoth"
-test4 = "mov coq + 5, d"
-test5 = 'mov n, ","+\',\''
-test6 = "mvi a, ';puu'"
-test7 = "snurfel"
-test8 = "sex 1,2,(3+a)"
-test9 = "db '[       ]', \"*  ;'-'  *\""
-test10 = "db '  \"'"
+class Parser:
+	ERR_UNTERMINATED_STRING = 'unterminated string literal'
 
-def tokenize(str, t1 = '', t2 = '', quote = None):
-	if len(t1) > 0:
-		yield t1
-	if len(t2) > 0:
-		yield t2
-	if quote != None and len(str) == 0:
-		raise Exception('unterminated string literal')
-	for i, c in enumerate(str):
-		print 'quote=', quote, ', c=', c
-		if c in '\'"':
-			for x in tokenize(str[i+1:], str[:i], str[i], c if quote == None else None):
-				yield x
-			break
-		if quote != None:
-			for x in tokenize(str[i+1:], str[:i], str[i], quote):
-				yield x
-			break
-		if c in ':,;()+-/*':
-			for x in tokenize(str[i+1:], str[:i], str[i]):
-				yield x
-			break
-		elif c in ' \t':
-			for x in tokenize(str[i+1:], str[:i], ''):
-				yield x
-			break
-		elif i == len(str) - 1:
-			yield str
+	@staticmethod
+	def tokenize(str, t1 = '', t2 = '', quote = None):
+		if len(t1) > 0:
+			yield t1
+		if len(t2) > 0:
+			yield t2
+		if quote != None and len(str) == 0:
+			raise Exception(Parser.ERR_UNTERMINATED_STRING)
+		for i, c in enumerate(str):
+			if quote != None and c != quote:
+				for x in Parser.tokenize(str[i+1:], str[:i], str[i], quote):
+					yield x
+				break
+			if c in '\'"':
+				for x in Parser.tokenize(str[i+1:], str[:i], str[i], c if quote == None else None):
+					yield x
+				break
+			if c in ':,;()+-/*':
+				for x in Parser.tokenize(str[i+1:], str[:i], str[i]):
+					yield x
+				break
+			elif c in ' \t':
+				for x in Parser.tokenize(str[i+1:], str[:i], ''):
+					yield x
+				break
+			elif i == len(str) - 1:
+				yield str
 
-def isInstruction(str):
-	return str in {"mov", "mvi", "sex", "db"}
+	@staticmethod
+	def joins(a1, a2):
+		#if a2 == []:
+		#	raise Exception('unterminated character literal')
+		return a1[0:-1] + [''.join([a1[-1]] + [a2])]
 
-def joins(a1, a2):
-	#if a2 == []:
-	#	raise Exception('unterminated character literal')
-	return a1[0:-1] + [''.join([a1[-1]] + [a2])]
+	@staticmethod
+	def args(head, tokens, expect = None):
+		#print tokens, next(tokens, []), expect
+		return (lambda car, cdr: 
+			Parser.args(Parser.joins(head, car), cdr, expect if car != expect else None) if expect != None 
+			else Parser.args(head + [car], cdr, car) if isinstance(car, basestring) and car in "'\""
+			else [head] if car == [] or car == ';'
+			else [head] + Parser.args([], cdr, expect) if car == ','
+			else Parser.args(head + [car], cdr, expect)) (next(tokens, []), tokens)
 
-def args(head, tokens, expect = None):
-	#print tokens, next(tokens, []), expect
-	return (lambda car, cdr: 
-		args(joins(head, car), cdr, expect if car != expect else None) if expect != None 
-		else args(head + [car], cdr, car) if isinstance(car, basestring) and car in "'\""
-		else [head] if car == [] or car == ';'
-		else [head] + args([], cdr, expect) if car == ','
-		else args(head + [car], cdr, expect)) (next(tokens, []), tokens)
+	def instr(self, instr, tokens, label = None):
+		return [label, instr, Parser.args([], tokens)]
 
-def instr(instr, tokens, label = None):
-	return [label, instr, args([], tokens)]
+	def parse(self, tokens, label = None):
+		return (lambda car, cdr:
+			self.instr(None, cdr, label) if car == ';' or car == ''
+			else self.parse(cdr, label) if car == ':'
+			else self.instr(car.upper(), cdr, label) if self.isInstruction(car)
+			else self.parse(cdr, label = car)) (next(tokens, ''), tokens)
 
-def parse(tokens, label = None):
-	return (lambda car, cdr:
-		instr(None, cdr, label) if car == ';' or car == ''
-		else parse(cdr, label) if car == ':'
-		else instr(car.upper(), cdr, label) if isInstruction(car)
-		else parse(cdr, label = car)) (next(tokens, ''), tokens)
+	def isInstruction(self, str):
+		return str in {"mov", "mvi", "sex", "db"}
 
-print [x for x in tokenize(test1)]
-print [x for x in tokenize(test2)]
-
-print [x for x in tokenize(test10)]
-
-print parse(tokenize(test1))
-print parse(tokenize(test2))
-print parse(tokenize(test3))
-print parse(tokenize(test4))
-print parse(tokenize(test5))
-print parse(tokenize(test6))
-print parse(tokenize(test7))
-print parse(tokenize(test8))
-print parse(tokenize(test9))
-print parse(tokenize(test10))
+	def Parse(self, line):
+		return self.parse(Parser.tokenize(line))
---
+++ projects/prettyasm/p2test.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+from p2 import Parser
+
+Tests = [
+	["db '", [], [], Parser.ERR_UNTERMINATED_STRING],
+	["bob: mov a, b 		; fuuu", 
+		['bob', ':', 'mov', 'a', ',', 'b', ';', 'fuuu'], 
+		['bob', 'MOV', [['a'], ['b']]]
+		],
+	["mike:mvi c, 'Q' 	; bob", 
+		['mike', ':', 'mvi', 'c', ',', "'", 'Q', "'", ';', 'bob'], 
+		['mike', 'MVI', [['c'], ["'Q'"]]]
+		],
+	["rob mvi c, 'Q'+8;ommog zoth", 
+		['rob', 'mvi', 'c', ',', "'", 'Q', "'", '+', '8', ';', 'ommog', 'zoth'], 
+		['rob', 'MVI', [['c'], ["'Q'", '+', '8']]]
+		],
+	["mov coq + 5, d", 
+		['mov', 'coq', '+', '5', ',', 'd'], 
+		[None, 'MOV', [['coq', '+', '5'], ['d']]]
+		],
+	['mov n, ","+\',\'', 
+		['mov', 'n', ',', '"', ',', '"', '+', "'", ',', "'"], 
+		[None, 'MOV', [['n'], ['","', '+', "','"]]]
+		],
+	["mvi a, ';puu'", 
+		['mvi', 'a', ',', "'", ';', 'p', 'u', 'u', "'"], 
+		[None, 'MVI', [['a'], ["';puu'"]]]
+		],
+	["snurfel", 
+		['snurfel'], 
+		['snurfel', None, [[]]]
+		],
+	["sex 1,2,(3+a)", 
+		['sex', '1', ',', '2', ',', '(', '3', '+', 'a', ')'], 
+		[None, 'SEX', [['1'], ['2'], ['(', '3', '+', 'a', ')']]]
+		],
+	["db '[       ]', \"*  ;'-'  *\"", 
+		['db', "'", '[', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ']', "'", ',', '"', '*', ' ', ' ', ';', "'", '-', "'", ' ', ' ', '*', '"'], 
+		[None, 'DB', [["'[       ]'"], ['"*  ;\'-\'  *"']]]
+		],
+ 	["db '  \"'", 
+ 		['db', "'", ' ', ' ', '"', "'"], 
+ 		[None, 'DB', [['\'  "\'']]]
+ 		],
+]
+
+testfail1 = "db '"
+
+def TestTokenizer(str, expect = None, exception = None):
+	try:
+		tokens = [x for x in Parser.tokenize(str)]
+		if (tokens != expect):
+			print 'TestTokenizer: FAIL %s expect=%s tokens=%s' % (repr(str), repr(expect), repr(tokens))
+		else:
+			print 'TestTokenizer: PASS %s' % repr(str)
+	except Exception as e:
+		if e.args[0] == exception:
+			print 'TestTokenizer: PASS %s -> %s' % (repr(str), repr(exception))
+			pass
+		else:
+			print 'Unexpected exception: ', e
+			raise e			
+
+def TestParser(str, expect = None, exception = None):
+	parser = Parser()
+	try:
+		expr = [x for x in parser.Parse(str)]
+		if (expr != expect):
+			print 'TestParser: FAIL %s expect=%s expr=%s' % (repr(str), repr(expect), repr(expr))
+		else:
+			print 'TestParser: PASS %s' % repr(str)
+	except Exception as e:
+		if e.args[0] == exception:
+			print 'TestParser: PASS %s -> %s' % (repr(str), repr(exception))
+			pass
+		else:
+			print 'Unexpected exception: ', e
+			raise e			
+
+for test in Tests:
+	TestTokenizer(test[0], test[1], test[3] if len(test) > 3 else None)
+
+for test in Tests:
+	TestParser(test[0], test[2], test[3] if len(test) > 3 else None)
+
+#TestTokenizer(test1)
+#TestTokenizer(test2)
+#TestTokenizer(test10)
+#TestTokenizer(testfail1, None, Parser.ERR_UNTERMINATED_STRING)
+
+#TestParser(test1)
+#TestParser(test2)
+#TestParser(test3)
+#TestParser(test4)
+#TestParser(test5)
+#TestParser(test6)
+#TestParser(test7)
+#TestParser(test8)
+#TestParser(test9)
+#TestParser(test10)
